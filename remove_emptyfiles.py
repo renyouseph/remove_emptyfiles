3d3ee8b8a880f62841525eb147e944e926192f4f
Reny Ouseph <renyouseph@gmail.com>
2018-07-16 20:30:06 +0530

    Add files via upload

---
+++ remove_emptyfiles.py
@@ -0,0 +1,12 @@
+### Requirements ###
+# apt-get update
+# apt-get install python3
+###
+
+import os
+
+for CurDir,SubDir,SubFiles in os.walk('/root/python/files'):
+    for files in SubFiles:
+        abPath = os.path.join(CurDir,files)
+        if os.path.getsize(abPath) == 0:
+            os.unlink(abPath)
